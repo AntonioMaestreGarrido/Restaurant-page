61d1eefb2547b666e196da38e285dfffdf097c3d
Antonio <antoniomaestregarrido.com>
2021-11-14 18:16:54 +0100

    menu done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 
 setListeners()
+//makeHome()
 
 
 
@@ -20,8 +21,7 @@ function setListeners() {
 
 
 function makeHome() {
-    if(document.querySelector("#cuerpo"))
-   { document.querySelector("#content").removeChild(document.querySelector("#cuerpo"))}
+    if (document.querySelector("#cuerpo")) { document.querySelector("#content").removeChild(document.querySelector("#cuerpo")) }
     var cuerpo = document.createElement("div")
     cuerpo.setAttribute("id", "cuerpo");
 
@@ -45,9 +45,32 @@ function makeHome() {
 
 
 function makeMenu() {
-    document.querySelector("#content").removeChild(document.querySelector("#cuerpo"))
-    alert("menu")
+    if (document.querySelector("#cuerpo")) { document.querySelector("#content").removeChild(document.querySelector("#cuerpo")) }
+    var cuerpo = document.createElement("div")
+    cuerpo.setAttribute("id", "cuerpo");
 
+    var fragment = new DocumentFragment();
+    const div1 = document.createElement("div");
+    fragment.appendChild(div1);
+
+    let h2 = document.createElement("h2");
+    h2.textContent = "NUESTRA CARTA";
+    div1.appendChild(h2);
+
+    let p = document.createElement("p");
+    p.textContent = "Desde este QR puedes descargar nuestra carta completa"
+    div1.appendChild(p);
+
+    let carta=document.createElement("img");
+    carta.setAttribute("src","../src/img/BAR-MORILES-CODIGO-QR.jpg");
+    div1.appendChild(carta);
+
+
+    
+
+
+    document.querySelector("#content").appendChild(cuerpo);
+    document.querySelector("#cuerpo").appendChild(fragment);
 }
 
 function makeContact() {
